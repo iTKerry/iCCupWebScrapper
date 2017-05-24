b401ff5ee2d70d7c37c8084d1c4bb25bef044c1c
Alex-Mac-WM <oleksandr.katrynets@devrecords.com>
2017-05-25 00:27:28 +0300

    Added match time

---
+++ iCCup.UI/Converters/MatchDateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace iCCup.UI.Converters
+{
+    public class MatchDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            return !str.Contains("today")
+                ? str.Substring(0, str.LastIndexOf('@') + 1).Replace(" @", String.Empty)
+                : str.Substring(str.LastIndexOf('@')).Replace("@ ", String.Empty);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
