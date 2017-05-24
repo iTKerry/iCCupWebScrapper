5e59eea851c1d240015992c97a75350c2ce2f025
Alex-Mac-WM <oleksandr.katrynets@devrecords.com>
2017-05-25 00:44:07 +0300

    Added MatchResultTextConverter

--- iCCup.UI/Converters/MatchResultTextConverter.cs
+++ iCCup.UI/Converters/MatchResultTextConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using iCCup.DATA.Models;
 
 namespace iCCup.UI.Converters
 {
@@ -8,7 +9,17 @@ namespace iCCup.UI.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            switch (value as MatchResult? ?? MatchResult.Restart)
+            {
+                case MatchResult.Win:
+                    return "Won Match";
+                case MatchResult.Lose:
+                    return "Lost Match";
+                case MatchResult.Leave:
+                    return "Abandoned";
+                default:
+                    return "No stats recorded";
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
